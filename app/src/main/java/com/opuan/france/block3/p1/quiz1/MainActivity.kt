9b8ecfa1483c8864e98741fa907f69a9572451f4
PransLoren <frdo.opu-an.up@phinmaed.com>
2023-12-07 17:12:27 +0800

    UI Fixed. To be fixed: Timer countdown ClickButton Function

---
+++ app/src/main/java/com/opuan/france/block3/p1/quiz1/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.opuan.france.block3.p1.quiz1
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var timer: TextView
+    lateinit var clicks: TextView
+
+    lateinit var btn_start: Button
+    lateinit var btn_click: Button
+
+    var currentTime = 10
+    var currentClicks = 0
+
+    lateinit var time: CountDownTimer
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        timer = findViewById(R.id.timer)
+        clicks = findViewById(R.id.clicks)
+
+        btn_start = findViewById(R.id.btn_start)
+        btn_click = findViewById(R.id.btn_click)
+
+        btn_click.isEnabled = false
+
+        btn_start.setOnClickListener{
+            currentTime = 10
+            currentClicks = 0
+
+        }
+
+        btn_click.setOnClickListener{
+
+        }
+
+        time = object : CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) {
+                currentTime--
+                val Tmr = currentTime + 1
+                timer.text = "Time: $Tmr"
+
+            }
+
+            override fun onFinish() {
+                timer.text = "Time: 0"
+
+                btn_start.isEnabled = true
+                btn_click.isEnabled = false
+            }
+
+        }
+    }
+
+}
